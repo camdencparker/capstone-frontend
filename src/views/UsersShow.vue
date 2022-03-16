15830aa22a6e31a754768e92b9e56f73a5f994e2
Camden <camdencparker@gmail.com>
2022-03-15 19:08:54 -0700

    usershow page, add user data with associated listing data

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -4,17 +4,18 @@ export default {
   data: function () {
     return {
       user: {},
+      listing: {},
       // currentUserId: localStorage.user_id
     };
   },
   created: function () {
     axios.get(`/users/1`).then((response) => {
-      this.listing = response.data;
-      console.log(this.listing);
+      this.user = response.data;
+      console.log(this.user);
     });
   },
   methods: {
-    destroyListing: function () {
+    destroyUser: function () {
       if (confirm("Are you sure you to delete this?")) {
         axios.delete(`/users/${this.user.id}`).then((response) => {
           console.log("Success", response.data);
@@ -28,10 +29,19 @@ export default {
 
 <template>
   <div class="users-show">
-    <h2>{{ user.name }}</h2>
-    <p>Email: {{ user.email }}</p>
-    <p>Phone: {{ user.phone }}</p>
-    <p>Location: {{ user.location }}</p>
+    <h2>{{ this.user.name }}</h2>
+    <img v-bind:src="this.user.profile_pic" v-bind:alt="this.user.id" />
+    <p>Email: {{ this.user.email }}</p>
+    <p>Phone: {{ this.user.phone }}</p>
+    <p>Location: {{ this.user.location }}</p>
+    <div v-for="listing in this.user.listings" v-bind:key="listing.id">
+      <p>{{ listing.brand }}</p>
+      <router-link v-bind:to="`/listings/${listing.id}`">
+        <img v-bind:src="this.listing.image_url" v-bind:alt="listing.id" />
+      </router-link>
+    </div>
+    <button v-on:click="destroyUser()">Delete Profile</button>
+    <br />
     <router-link to="/listings">To all listings</router-link>
   </div>
 </template>
